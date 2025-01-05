4d8264c6ee46c57f7e19927bd0cb579c9d668bdc
mohamed.sabbah <mohamed.sabbah@gizasystems.com>
2025-01-05 11:48:04 +0200

    init

---
+++ src/main/java/io/spring/batch/bank_batch_service/BankBatchServiceApplication.java
@@ -0,0 +1,13 @@
+package io.spring.batch.bank_batch_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankBatchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankBatchServiceApplication.class, args);
+	}
+
+}
